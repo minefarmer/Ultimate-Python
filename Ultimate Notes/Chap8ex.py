4c52b2b31c09e993c6c1f73e8610ad7a864dfefe
C. Richard Matson <pgoldfarm@yahoo.com>
2020-07-10 11:47:43 -0500

    "Ultimate Notes/Chap8ex.py": create backt to linux  and python2

---
+++ Ultimate Notes/Chap8ex.py
@@ -0,0 +1,19 @@
+# Exercize 1   6
+# Exercise 2   20
+# 
+# 
+# 
+# # 5! = 5*4*3*2*1 = 120
+# # 6! = 6 * 5!
+# # factorial computation iteratively
+# print("Enter a number: ")  # Enter a number:
+# num = int(input())  # 5
+# fact = 1
+# for i in range(1, num+1):
+#     fact = fact * i
+# print(str(num) + '! = ' + str(fact))  # 5! = 120
+
+
+
+
+
