5aec7fcb8e432b7f2e095a0704fcd08a73abdb6e
minefarmer <pgoldfarm@yahoo.com>
2020-07-13 22:33:39 -0500

    "Ultimate Notes/Master Notes/InheritDrivedClass.py": create not workingBye

---
+++ Ultimate Notes/Master Notes/InheritDrivedClass.py
@@ -0,0 +1,14 @@
+# class Employee:  *********************  not working
+#     def __init__(self, name, payRate):
+#         self.name = name
+#         self.payRate = payRate
+        
+#     def __str__(self):
+#         return self.name + ', ' + str(self.payRate)
+    
+#     def pay(self, payrate, hoursWorked):
+#         return self.payrate * hoursWorked
+    
+# el = Employee('John Jones', 10.00)
+# print(el)
+# print('Gross pay: ' + str(el.pay(40)))
